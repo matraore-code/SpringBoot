bd3c0927d46c85cede9ef53b5c5bdebbd1c9f5d5
matraore-code <mamadou.moctartra@gmail.com>
2023-02-08 00:31:02 +0100

    creating customer repository

--- src/main/java/com/matraore/CustomerRepository.java
+++ src/main/java/com/matraore/CustomerRepository.java
@@ -3,5 +3,5 @@ package com.matraore;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface CustomerRepository extends JpaRepository<Customer, Integer> {
-    
+
 }
--- src/main/java/com/matraore/Main.java
+++ src/main/java/com/matraore/Main.java
@@ -3,19 +3,31 @@ package com.matraore;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
 @SpringBootApplication
 @RestController
+@RequestMapping("api/v1/customers")
 public class Main {
+    private final CustomerRepository customerRepository;
+
+    public Main(CustomerRepository customerRepository1) {
+        this.customerRepository = customerRepository1;
+    }
     public static void main(String[] args) {
         //System.out.println("Hello World");
         SpringApplication.run(Main.class, args);
     }
 
-    @GetMapping("/")
+    @GetMapping
+    public List<Customer>getCustomers(){
+        return customerRepository.findAll();
+    }
+
+    /*@GetMapping("/")
     public GreetResponse greet() {
         GreetResponse greetResponse = new GreetResponse(
                 "Hello",
@@ -34,5 +46,5 @@ public class Main {
 
     ) {
 
-    }
+    }*/
 }
